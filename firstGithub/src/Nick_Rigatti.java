cc5868ffca0f0ec9f779e66c44766a243184898e
nwalczak99 <113116376+nwalczak99@users.noreply.github.com>
2024-02-20 12:19:51 -0500

    Hello Nick :3

--- firstGithub/src/Nick_Rigatti.java
+++ firstGithub/src/Nick_Rigatti.java
@@ -1,2 +1,8 @@
 public class Nick_Rigatti {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Nick!");
+        System.out.println("Hello Nick :3");
+        System.out.println("Hello Priyum :3");
+    }
 }
