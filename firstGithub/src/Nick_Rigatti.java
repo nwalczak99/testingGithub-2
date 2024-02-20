abf0edc3dd710e6e24bf886254f632165ddf2516
nwalczak99 <113116376+nwalczak99@users.noreply.github.com>
2024-02-20 12:21:13 -0500

    Age

--- firstGithub/src/Nick_Rigatti.java
+++ firstGithub/src/Nick_Rigatti.java
@@ -4,5 +4,9 @@ public class Nick_Rigatti {
         System.out.println("Hello Nick!");
         System.out.println("Hello Nick :3");
         System.out.println("Hello Priyum :3");
+        for(int i = 0;i < 16;i++)
+        {
+            System.out.println("I am " + i + " years old.");
+        }
     }
 }
